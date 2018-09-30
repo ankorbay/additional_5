62c1c65df4d1791ba742a0e99e0cac1666194eab
Anton M Korshunov <anton.m.korshunov@gmail.com>
2018-09-30 12:35:37 +0300

    Brackets task (#6) Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let o_br = [];
+  let cl_br = [];
+  let temp = [];
+  let counter = 0;
+  for(let i=0; i<bracketsConfig.length; i++)
+  {
+    o_br[i] = bracketsConfig[i][0];
+    cl_br[i] = bracketsConfig[i][1];
+  }
+  for (let i=0; i<str.length;i++)
+  {
+    for(let j=0;j<o_br.length; j++)
+    {
+      if(o_br[j]===cl_br[j] && str[i]===cl_br[j])
+      {
+        if(str[i]===cl_br[temp[temp.length-1]])
+        {
+          temp.pop();
+          counter--;
+        }
+        else
+        {
+          temp.push(j);
+          counter++;
+        }
+      }
+      else if(str[i]===o_br[j])
+      {
+        temp.push(j);
+        counter++;
+      }
+      else if(str[i]===cl_br[j])
+      {
+        if(j===npmtemp.pop())
+        {
+          counter--;
+        }
+        else return false;
+      }
+    }
+    if(counter < 0) return false;
+  }
+  if(counter === 0) return true;
+  else return false;
 }
